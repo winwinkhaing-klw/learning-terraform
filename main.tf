56b39f21762a1bc1522d675fe66481db2bbaeb92
Win Win Khaing <40992531+winwinkhaing-klw@users.noreply.github.com>
2024-02-15 13:25:47 +0700

    Update Security group

--- main.tf
+++ main.tf
@@ -39,16 +39,16 @@ resource "aws_security_group_rule" "blog_http_in" {
   type                   = "ingress"
   from_port              = 80
   to_port                = 80
-  cidr_blocks            = [0.0.0.0/0]
+  cidr_blocks            = ["0.0.0.0/0"]
   aws_security_group_id  = aws_security_group.blog.id
 }
 
 
-resource "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type                   = "ingress"
   from_port              = 443
   to_port                = 443
-  cidr_blocks            = [0.0.0.0/0]
+  cidr_blocks            = ["0.0.0.0/0"]
   aws_security_group_id  = aws_security_group.blog.id
 }
 
