42905b655a9d413508ec0faa0ae48b6ec86c96d6
Win Win Khaing <40992531+winwinkhaing-klw@users.noreply.github.com>
2024-02-16 11:31:56 +0700

    auto scaling

--- main.tf
+++ main.tf
@@ -33,16 +33,21 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+
+  security_groups = [module.blog_sg.security_group_id]
+  images_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
+  # insert the 1 required variable here
 }
 
 module "alb" {
